0d0230384175581700500fdef1a7fd5416d2e0dc
SaniaaSharmaa <sania.98.sharma@hotmail.com>
2023-05-19 23:19:13 +0200

    udon maruka

--- src/assets/data/FoodData.ts
+++ src/assets/data/FoodData.ts
@@ -2,52 +2,52 @@
 
 export const FoodData = [
   {
+    name: "Sushi Miyui",
     location: "1F Maki Plaza,, 1-15-11 Shintomi",
     city: "Tokyo",
     category: "Sushi 🍣",
     image:
       "https://res.cloudinary.com/dowiygzq3/image/upload/v1684446337/mainImage_hmgbrh.jpg",
-    name: "Sushi Miyui",
     priceRange: 1,
   },
 
   {
+    name: "Sukiyabashi Jiro Roppongi",
     location: "〒106-0032 Tokyo, Minato City, Roppongi, 6 Chome−12−2, 3F",
     city: "Tokyo",
     category: "Sushi 🍣",
     image:
       "https://res.cloudinary.com/dowiygzq3/image/upload/v1684447294/jiro-roppongi-min_l9x5vy.jpg",
-    name: "Sukiyabashi Jiro Roppongi",
     priceRange: 3,
     info: "A legend among sushi legends, Sukibayashi Jiro (すきやばし次郎) plays in a league of his own. As the first restaurant in Japan to achieve 3 Michelin stars, a popular documentary film further catapulted the restaurant to larger-than-life stardom, to the point that scoring a seat became such a daunting task, that it stopped appearing on the popular guide as of 2020. But the legendary name quickly returned to the guide´s pages with none other than 2 stars, under a different branch opened in Tokyo´s upscale Roppongi district. Helmed by Jiro Ono´s second son Takashi Ono, this location stays on brand with the same exceptional quality that made its predecessor a worldwide celebrity. ",
   },
   {
+    name: "Ginza Harutaka",
     location: "〒104-0061 Tokyo, Chuo City, Ginza, 8 Chome−3−1 6階",
     city: "Tokyo",
     category: "Sushi 🍣",
     image:
       "https://res.cloudinary.com/dowiygzq3/image/upload/v1684447859/BestSushi-Harutaka_zg3zp3.jpg",
-    name: "Ginza Harutaka",
     priceRange: 3,
     info: "Harutaka (青空), located in Tokyo’s exclusive Ginza district, is a premier sushi restaurant run by chef Harutaka Takahashi, a master in the craft of high-quality sushi. After all, his training took place at the aforementioned Sukibayashi Jiro, so it surprises no one that the skills displayed at his own restaurant have resulted in two Michelin stars for Harutaka. Experiencing the complexity of taste and texture of the most authentic sushi, Chef Takahashi elevates sushi to an art form. Diners at the counter are surrounded by a minimalist yet warm ambiance and have the unique experience of watching the chef prepare each piece in front of them.",
   },
   {
+    name: "Uogashi Nihon-ichi Shibuya Dogenzaka",
     location: "2 Chome-9-1 Dogenzaka, Shibuya City, Tokyo 150-0043, Japan",
     city: "Tokyo",
     category: "Sushi 🍣",
     image:
       "https://res.cloudinary.com/dowiygzq3/image/upload/v1684447971/image_ds0cdr.png",
-    name: "Uogashi Nihon-ichi Shibuya Dogenzaka",
     priceRange: 1,
     info: "Visiting a standing sushi bar is a must while you’re in Tokyo and Uogashi Nihon-Ichi is one of the best around. You’ll find this chain eatery in a few areas around Tokyo, including its newly reopened outlet in Shibuya Dogenzaka. The best part about visiting this joint is watching the sushi chefs up close as they whip up your order at lightning speed. There’s an English menu – or you can just point to the seafood you recognise at the counter.",
   },
 
   {
+    name: "Udon Maruka",
     location:
       "1F New Surugadai bldg., 3-16-1 Kanda-ogawa-machi, Chiyoda-ku, Tokyo, 101-0052",
     city: "Tokyo",
     category: "Udon 🍜",
-    name: "",
     image:
       "https://res.cloudinary.com/deiaf96eq/image/upload/v1684529445/i-qRRgKm4-XL_as5cip.jpg",
     image1:
@@ -55,7 +55,7 @@ export const FoodData = [
     image2:
       "https://res.cloudinary.com/deiaf96eq/image/upload/v1684529636/p14-swinnerton-maruka-y-20170716_gwmfuq.jpg",
     priceRange: 1,
-    info: "Udon Maruka in Kanda is by far the best Sanuki udon restaurant in Tokyo, and you´ll have to be prepared to queue for the pleasure of slurping these thick, chewy noodles. If you don´t mind standing in line for 45 minutes or more, you´ll be rewarded with some of the best Sanuki udon noodles in Tokyo. Sanuki udon is a Kagawa prefecture specialty, and noodle aficionados in Tokyo swear by Udon Maruka. since you´ll be waiting in line, you´ll have plenty of time to decide exactly how you want your udon - hot or cold, with soup or without, and all the toppings you´d like on it.",
+    info: "Udon Maruka in Kanda is by far the best Sanuki udon restaurant in Tokyo, and you´ll have to be prepared to queue 45 minutes or more for the pleasure of slurping these thick, chewy noodles. Noodle enthusiasts in Tokyo swear by Udon Maruka.",
   },
 
   {
