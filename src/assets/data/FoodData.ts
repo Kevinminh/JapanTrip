0f7fdb0c15f559c56e88e00bf4e5a448502a2e96
SaniaaSharmaa <sania.98.sharma@hotmail.com>
2023-05-20 00:28:51 +0200

    tokyo food almost done

--- src/assets/data/FoodData.ts
+++ src/assets/data/FoodData.ts
@@ -6,8 +6,9 @@ export const FoodData = [
     location: "1F Maki Plaza,, 1-15-11 Shintomi",
     city: "Tokyo",
     category: "Sushi 🍣",
-    image:
+    galleryImages: [
       "https://res.cloudinary.com/dowiygzq3/image/upload/v1684446337/mainImage_hmgbrh.jpg",
+    ],
     priceRange: 1,
   },
 
@@ -16,8 +17,9 @@ export const FoodData = [
     location: "〒106-0032 Tokyo, Minato City, Roppongi, 6 Chome−12−2, 3F",
     city: "Tokyo",
     category: "Sushi 🍣",
-    image:
+    galleryImages: [
       "https://res.cloudinary.com/dowiygzq3/image/upload/v1684447294/jiro-roppongi-min_l9x5vy.jpg",
+    ],
     priceRange: 3,
     info: "A legend among sushi legends, Sukibayashi Jiro (すきやばし次郎) plays in a league of his own. As the first restaurant in Japan to achieve 3 Michelin stars, a popular documentary film further catapulted the restaurant to larger-than-life stardom, to the point that scoring a seat became such a daunting task, that it stopped appearing on the popular guide as of 2020. But the legendary name quickly returned to the guide´s pages with none other than 2 stars, under a different branch opened in Tokyo´s upscale Roppongi district. Helmed by Jiro Ono´s second son Takashi Ono, this location stays on brand with the same exceptional quality that made its predecessor a worldwide celebrity. ",
   },
@@ -26,8 +28,9 @@ export const FoodData = [
     location: "〒104-0061 Tokyo, Chuo City, Ginza, 8 Chome−3−1 6階",
     city: "Tokyo",
     category: "Sushi 🍣",
-    image:
+    galleryImages: [
       "https://res.cloudinary.com/dowiygzq3/image/upload/v1684447859/BestSushi-Harutaka_zg3zp3.jpg",
+    ],
     priceRange: 3,
     info: "Harutaka (青空), located in Tokyo’s exclusive Ginza district, is a premier sushi restaurant run by chef Harutaka Takahashi, a master in the craft of high-quality sushi. After all, his training took place at the aforementioned Sukibayashi Jiro, so it surprises no one that the skills displayed at his own restaurant have resulted in two Michelin stars for Harutaka. Experiencing the complexity of taste and texture of the most authentic sushi, Chef Takahashi elevates sushi to an art form. Diners at the counter are surrounded by a minimalist yet warm ambiance and have the unique experience of watching the chef prepare each piece in front of them.",
   },
@@ -36,8 +39,9 @@ export const FoodData = [
     location: "2 Chome-9-1 Dogenzaka, Shibuya City, Tokyo 150-0043, Japan",
     city: "Tokyo",
     category: "Sushi 🍣",
-    image:
+    galleryImages: [
       "https://res.cloudinary.com/dowiygzq3/image/upload/v1684447971/image_ds0cdr.png",
+    ],
     priceRange: 1,
     info: "Visiting a standing sushi bar is a must while you’re in Tokyo and Uogashi Nihon-Ichi is one of the best around. You’ll find this chain eatery in a few areas around Tokyo, including its newly reopened outlet in Shibuya Dogenzaka. The best part about visiting this joint is watching the sushi chefs up close as they whip up your order at lightning speed. There’s an English menu – or you can just point to the seafood you recognise at the counter.",
   },
@@ -45,23 +49,77 @@ export const FoodData = [
   {
     name: "Udon Maruka",
     location:
-      "1F New Surugadai bldg., 3-16-1 Kanda-ogawa-machi, Chiyoda-ku, Tokyo, 101-0052",
+      "〒1F New Surugadai bldg., 3-16-1 Kanda-ogawa-machi, Chiyoda-ku, Tokyo, 101-0052",
     city: "Tokyo",
-    category: "Udon 🍜",
-    image:
+    category: "Udon 🍲",
+    galleryImages: [
       "https://res.cloudinary.com/deiaf96eq/image/upload/v1684529445/i-qRRgKm4-XL_as5cip.jpg",
-    image1:
       "https://res.cloudinary.com/deiaf96eq/image/upload/v1684529560/i-2Zd68R6-XL_c16nug.jpg",
-    image2:
       "https://res.cloudinary.com/deiaf96eq/image/upload/v1684529636/p14-swinnerton-maruka-y-20170716_gwmfuq.jpg",
+    ],
     priceRange: 1,
     info: "Udon Maruka in Kanda is by far the best Sanuki udon restaurant in Tokyo, and you´ll have to be prepared to queue 45 minutes or more for the pleasure of slurping these thick, chewy noodles. Noodle enthusiasts in Tokyo swear by Udon Maruka.",
+    openingHours:
+      "Monday-Friday: 11:00 - 16:00 & 17:00 - 19:30, Saturday: 11:00 - 14:30 and closed on Sunday",
   },
 
   {
-    category: "Curry 🍛",
-    name: "",
-    location: "",
+    name: "Hinadoriyama",
+    location: "〒1602-4 Kamiyugi, Hachioji, Tokyo 192-0373, Japan",
+    city: "Tokyo",
+    category: "Generelt",
+    galleryImages: [
+      "https://res.cloudinary.com/deiaf96eq/image/upload/v1684531591/Hinatoriyama-Japanese-conveyor-belt-boat-train-revolving-restaurat-Tokyo-Hachioji-noodles-best-food-review-photos-13.jpg_olycnj.webp",
+      "https://res.cloudinary.com/deiaf96eq/image/upload/v1684531635/640x640_rect_69269332_pjybox.jpg",
+    ],
+    priceRange: 3,
+    info: "Should book before going",
+    openingHours: "11:00 - 20:30, closed on Wednesdays",
+  },
+
+  {
+    name: "Ichiran Ramen",
+    location: "〒150-0041 B1F, 1-22-7 Jinnan Sibuya-ku Tokyo-to",
+    city: "Tokyo",
+    category: "Ramen 🍜",
+    galleryImages: [
+      "https://res.cloudinary.com/deiaf96eq/image/upload/v1684533124/image.jpg_ghmr81.png",
+      "https://res.cloudinary.com/deiaf96eq/image/upload/v1684533147/img_6793-3.jpg_axuint.webp,",
+    ],
+    priceRange: 1,
+    info: "Famous ramen restaurant. Here you sit in a sort of box where your ramen is provided through a small shutter. Prices from NOK 63.",
+    openingHours: "10:00 - 06:00",
+  },
+
+  {
+    name: "Torikizoku",
+    location: "〒150-0042 Tokyo, Shibuya City, Udagawacho, 34−5 ２F",
+    city: "Tokyo",
+    category: "BBQ 🍢",
+    galleryImages: [
+      "https://res.cloudinary.com/deiaf96eq/image/upload/v1684533906/images_titwej.jpg",
+      "https://res.cloudinary.com/deiaf96eq/image/upload/v1684533954/s_0ooh.jpg_pop52s.webp",
+      "https://res.cloudinary.com/deiaf96eq/image/upload/v1684533948/jumbo-yakitori-torikizoku_bjrxck.jpg",
+    ],
+    priceRange: 1,
+    info: "Should make reservations. This place is known for chicken. The meat is exceptionally good and comes with an amazing potato salad and free refill of cabbage.",
+    openingHours:
+      "Monday & Tuesday: 17:00 - 04:00, Wednesday: 16:00 - 04:00, Thursday & Friday: 15:00 - 04:00 and closed on Saturday & Sunday",
+  },
+
+  {
+    name: "Oreryu Shio Ramen",
+    location: "1 Chome-22-8 Dogenzaka, Shibuya City, Tokyo 150-0043, Japan",
+    city: "Tokyo",
+    category: "Ramen 🍜",
+    galleryImages: [
+      "https://res.cloudinary.com/deiaf96eq/image/upload/v1684534458/c9032c_2b1245ef19824bf18b28ec4d7fddae3f_mv2.jpg_zb2a57.webp",
+      "https://res.cloudinary.com/deiaf96eq/image/upload/v1684534512/20190526-205726-largejpg_uwrbbw.jpg",
+      "https://res.cloudinary.com/deiaf96eq/image/upload/v1684534549/la-macchinetta-al-di_lrs9bj.jpg",
+    ],
+    priceRange: 2,
+    info: "It´s close to the shopping area. Food selection and payment is made at the 'ATM machine' outside.",
+    openingHours: "11:00 - 23:00",
   },
   {
     category: "Wagyu 🥩",
